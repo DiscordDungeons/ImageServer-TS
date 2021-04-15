9942e2f0617f0867e5133156904480723c8871df
Snazzah <suggesttosnazzy@gmail.com>
2021-04-15 03:03:12 -0500

    Fix linting and indents

--- src/types.ts
+++ src/types.ts
@@ -29,9 +29,9 @@ export type IQLReturnAction = ReturnImageAction
 /** Loads an image into the script execution. */
 export interface LoadImageAction {
 	ActionType: "LOAD_IMAGE",
-  /** The URL from which to load the image */
+	/** The URL from which to load the image */
 	Url: string,
-  /** The name used to track the image */
+	/** The name used to track the image */
 	ImageName: string,
 }
 
@@ -41,55 +41,55 @@ export interface LoadImageAction {
  */
 export interface LoadSpritesAction {
 	ActionType: "LOAD_SPRITES",
-  /** The URL from which to load the image */
+	/** The URL from which to load the image */
 	Url: string,
-  /** The name used to track the image */
+	/** The name used to track the image */
 	ImageName: string,
-  /** 	The properties for the action */
-  Properties: {
-    /** The size of a sprite */
-    SpriteSize: [number, number],
-  },
+	/** The properties for the action */
+	Properties: {
+		/** The size of a sprite */
+		SpriteSize: [number, number],
+	},
 }
 
 /** ?odifies an image. */
 export interface ModifyImageAction {
 	ActionType: "MODIFY_IMAGE",
-  /** The name of the image to modify */
+	/** The name of the image to modify */
 	ImageName: string,
-  /** The properties to modify */
-  Properties: {
-    /** Makes an image grayscale */
-    Grayscale?: boolean | {
-      /** Whether to include transparency in grayscaling */
-      IncludeTransparency: boolean,
-    },
-    /** Pastes a loaded image at a given position */
-    PasteImage?: {
-      /** The name of the image to paste */
-      ImageName: string,
-      /** The X and Y coordinates to paste the image at */
-      PasteAt: [number, number],
-    },
-    /** Inverts an image */
-    Invert?: boolean,
-  }
+	/** The properties to modify */
+	Properties: {
+		/** Makes an image grayscale */
+		Grayscale?: boolean | {
+			/** Whether to include transparency in grayscaling */
+			IncludeTransparency: boolean,
+		},
+		/** Pastes a loaded image at a given position */
+		PasteImage?: {
+			/** The name of the image to paste */
+			ImageName: string,
+			/** The X and Y coordinates to paste the image at */
+			PasteAt: [number, number],
+		},
+		/** Inverts an image */
+		Invert?: boolean,
+	}
 }
 
 /** Creates a new image. */
 export interface NewImageAction {
 	ActionType: "NEW_IMAGE",
-  /** The name of the image to modify */
+	/** The name of the image to modify */
 	ImageName: string,
-  /** The properties to modify */
-  Properties: {
-    Size: [number, number],
-  },
+	/** The properties to modify */
+	Properties: {
+		Size: [number, number],
+	},
 }
 
 /** Returns an image. */
 export interface ReturnImageAction {
 	ActionType: "RETURN_IMAGE",
-  /** The name of the image to return */
+	/** The name of the image to return */
 	ImageName: string,
 }
